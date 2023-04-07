e7f01240902adb5103c99d28410835e3820c2768
bzamparo80 <bzamparo@gmail.com>
2023-04-07 20:28:20 -0300

    buynow

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Carrusel from "./components/Carousel";
 import Footer from "./components/Footer.js";
 import Navbar from "./components/Navbar.js";
+import BuyNow from "./components/BuyNow";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <h1>Info</h1>
       <h1>About Us</h1>
       <h1>Call to action</h1>
-      <h1>Buy Now</h1>
+      <BuyNow />
       <Carrusel />
       <h1>Redes Sociales</h1>
       <Footer />
---
+++ client/src/components/BuyNow.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import buyNow from "../image/buyNow.png"
+import "../stylesheet/BuyNow.css"
+
+const BuyNow = () => {
+  return (
+    <div className="buyNow">
+        <div className="imagen">
+            <img style = {{width: "400px"}}src={buyNow} alt="buyNow" />
+        </div>
+        <div className="action">
+            <p>Ofrecemos una amplia variedad de pastas frescas y deliciosas elaboradas con ingredientes de alta calidad. Desde opciones sin gluten hasta pastas veganas y vegetarianas, nos enorgullece ofrecer pastas que satisfacen cualquier necesidad dietética o preferencia. Además, nos preocupamos por el medio ambiente y utilizamos empaques biodegradables y fuentes de energía renovable. </p>
+            <a href="#tienda">Ir a la tienda</a>            
+        </div>
+    </div>
+  )
+}
+
+export default BuyNow

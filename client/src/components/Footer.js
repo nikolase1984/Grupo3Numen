f4ee430c037c75f19f573d7a93e6c267f8290345
bzamparo80 <bzamparo@gmail.com>
2023-04-10 18:28:44 -0300

    nuevasMedidasFooter

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,15 +10,15 @@ const Footer = () => {
 
     <div>
       <div className="logo">
-          <img style={{height: "80px", width: "80px"}}src= {logoRojo} alt="logo"/>
+          <img style={{height: "120px", width: "120px"}}src= {logoRojo} alt="logo"/>
       </div>
     </div>
 
     <div className="redes">
       <div>
-          <a className="fb" href="https://www.facebook.com" target="blank"><FaFacebook style={{color: "#FFC03D", fontSize: "25px"}}/></a>
-          <a className="ig" href="https://www.instagram.com" target="blank"><RiInstagramFill style={{color: "#FF2525", fontSize: "25px"}}/></a>
-          <a className="wa" href="https://www.whatsapp.com" target="blank"><RiWhatsappFill style={{color: "#432344", fontSize: "25px"}}/></a>
+          <a className="fb" href="https://www.facebook.com" target="blank"><FaFacebook style={{color: "#FFC03D", fontSize: "35px"}}/></a>
+          <a className="ig" href="https://www.instagram.com" target="blank"><RiInstagramFill style={{color: "#FF2525", fontSize: "35px"}}/></a>
+          <a className="wa" href="https://www.whatsapp.com" target="blank"><RiWhatsappFill style={{color: "#432344", fontSize: "35px"}}/></a>
       </div>
     </div>
  

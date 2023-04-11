a90cd94dcd5b395153a7cade0b083d9e97b99872
exequiel <exequielagustindefreitas@gmail.com>
2023-04-11 00:16:35 -0300

    about us imagen

--- client/src/components/Aboutus.js
+++ client/src/components/Aboutus.js
@@ -1,36 +1,31 @@
 import '../stylesheet/Aboutus.css'
-import tenedorAbre from '../image/tenedor.png';
+import tenedorAbre from '../image/pngwing.com.png';
 import tenedorCierra from '../image/tenedor (1).png';
 
 const AboutUs = () => {
   return (
     <div className='main'>
-    <div className='nosotros'>
-    <h3 className='tituloAU'>Conoce nuestra historia</h3>
-    <div className='info'>
-        
-
-
-    <div className='div2'>
-         <img className='apertura'   />
-         <ul>
-         <li>Somos un grupo de 5 amigos que abrimos una fabrica de pastas con los mejores 
- sabemos de Italia. Hace 15 años que empezamos este hermoso proyecto, que hoy en dia ha crecido
- muchisimo y nos permite tener un gran emprendimiento.</li>
- <li>Nuestro nombre surgio debido a la nacionalidad de nuestros abuelos, siendo italianos.
- En nuestro pais, tenemos la costumbre de todo aquel con origenes y/o familiares de italia, se 
- acostumbra de llamarle "tano", por eso mismo nuestra franquicia se llama asi.  
- </li>
- <li>Actualmente tenemos 15 locales con venta presenciales distribuidos a lo largo del pais </li>
- <li>Contamos con venta al por mayor, por menor y totalmente online.</li>
- <li>Envios sin cargo en un rango de 150 kilometros</li>
-</ul>
+      <div className='nosotros'>
+        <h3 className='tituloAU'>Conoce nuestra historia</h3>
+      <div className='info'>  
+        <div className='div2'>
+        <ul>
+          <li>Somos un grupo de 5 amigos que abrimos una fabrica de pastas con los mejores 
+          sabemos de Italia. Hace 15 años que empezamos este hermoso proyecto, que hoy en dia ha crecido
+          muchisimo y nos permite tener un gran emprendimiento.</li>
+          <li>Nuestro nombre surgio debido a la nacionalidad de nuestros abuelos, siendo italianos.
+          En nuestro pais, tenemos la costumbre de todo aquel con origenes y/o familiares de italia, se 
+          acostumbra de llamarle "tano", por eso mismo nuestra franquicia se llama asi.  
+          </li>
+          <li>Actualmente tenemos 15 locales con venta presenciales distribuidos a lo largo del pais </li>
+          <li>Contamos con venta al por mayor, por menor y totalmente online.</li>
+          <li>Envios sin cargo en un rango de 150 kilometros</li>
+        </ul>
+        <img className='cocinero' src={tenedorAbre} alt="caricatura cocinero" />
+      </div>   
+      </div>
     </div>
-    
-
-    </div>
-    </div>
-    </div>
+  </div>
   )
 }
 

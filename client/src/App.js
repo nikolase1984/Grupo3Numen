b107a978cb2b79f4be0b54841a6c2b415ac70f6d
bzamparo80 <bzamparo@gmail.com>
2023-04-12 20:57:57 -0300

    actInfo

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import CardList from "./components/CardList";
 
 
 
+
 function App() {
   return (
     <div className="App">
@@ -57,6 +58,7 @@ function App() {
       <h1>Call to action</h1>
       <BuyNow />
       <Carrusel />
+      <Info />
       <Footer />
     </div>
   );

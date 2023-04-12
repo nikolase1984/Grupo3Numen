da5e5c71a4800cf1ab4be833276df8f21c35f887
Ariel Basabe <lazurus.kilix@gmail.com>
2023-04-12 19:41:45 -0300

    actualizacion3

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+
 import "./App.css";
 import AboutUs from "./components/Aboutus.js";
 import Carrusel from "./components/Carousel";
@@ -5,20 +6,47 @@ import Footer from "./components/Footer.js";
 import Navbar from "./components/Navbar.js";
 import BuyNow from "./components/BuyNow";
 import Imagenconboton from "./components/Imagen-con-boton";
-import Info from "./components/Info";
+import Pastas from "./components/Cards";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Imagenconboton />
-      <h1>Tarjetas</h1>
+      
+    <Pastas
+    nombre= 'Sorrentinos'
+    sabor='Mozzarella, Jamon'
+    imagen='sorrentinos'
+    rendimiento=' 1 plancha por persona.
+    '/>
+
+    <Pastas
+    nombre='Ravioles'
+    sabor='Verdura y Pollo'
+    imagen='Ravioles'
+    rendimiento='1 plancha por persona.
+    '/>
+
+    <Pastas 
+    nombre='Ñoquis'
+    sabor='Papa - Espinaca'
+    imagen='Nioquis'
+    rendimiento='300g por persona'
+    />
+
+    <Pastas 
+    nombre='Fideos'
+    sabor='Semola - Espinaca'
+    imagen='Fideos'
+    rendimiento='200g por persona'
+    />
+
       <h1>Info</h1>
       <AboutUs/>
       <h1>Call to action</h1>
       <BuyNow />
       <Carrusel />
-      <Info />      
       <Footer />
     </div>
   );

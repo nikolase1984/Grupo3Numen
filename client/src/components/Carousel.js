5414b7486a08940e283ca560e1341caa998e2539
exequiel <exequielagustindefreitas@gmail.com>
2023-04-12 19:13:38 -0300

    cambios responsive cocinero

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -6,14 +6,14 @@ export function Carrusel() {
     <div>
       <Carousel>
         <Carousel.Item>
-          <img
+          <img 
             className="d-block"
             src="https://hips.hearstapps.com/prima.cdnds.net/assets/15/26/1435331606-different-pasta-shapes.jpg"
             alt="First slide"
           />
         </Carousel.Item>
         <Carousel.Item>
-          <img
+          <img 
             className="d-block"
             src="https://assets.epicurious.com/photos/606b6eb818ac0b1841b3bbbf/6:4/w_4526,h_3017,c_limit/WackyPastaShapes_HERO_040121_12258.jpg"
             alt="Second slide"
@@ -21,7 +21,7 @@ export function Carrusel() {
 
         </Carousel.Item>
         <Carousel.Item>
-          <img
+          <img 
             className="d-block"
             src="https://www.finedininglovers.it/sites/g/files/xknfdk1106/files/2022-06/tipi%20di%20pasta%20-%20Finedininglovers.jpg"
             alt="Third slide"

0b4f26f2c5f45f08c58135a810ab0e8a2c4c6932
Nico Escobar <nikolase@gmail.com>
2023-04-23 22:11:50 -0300

    arreglos imagen con boton

--- src/components/Imagen-con-boton.js
+++ src/components/Imagen-con-boton.js
@@ -3,7 +3,7 @@ import imagen from '../image/imagen-con-boton3.jpg'
 
 const Imagenconboton = () => {
   return (
-    <div className='flex flex-col overflow-hidden md:flex-row md:w-screen ' >
+    <div className='flex flex-col overflow-hidden md:flex-row lg:w-screen ' >
     <div className= 'font-serif italic h-1/2 bg-celeste md:w-1/2 md:h-auto'>
       <h2 className='my-20 text-2xl text-center text-red-600 sm:mx-2 xl:text-4xl xl:text-left xl:px-4' >La tradición de la pasta italiana, hecha con nuestras manos</h2>
       <button className='w-auto mb-16 text-sm duration-300 bg-violeta md:hover:scale-105 hover:ease-linear lg:mt-12'>CONOCE NUESTRAS RECETAS</button>      

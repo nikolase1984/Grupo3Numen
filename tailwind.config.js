c1539c7be00765bd38680838ac60257c8a5cd075
bzamparo80 <bzamparo@gmail.com>
2023-04-23 23:57:56 -0300

    modiftw2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        violet: '#432344',
-        red: '#FF2525',
-        grey: '#E7EEFB',
-        yellow: '#FFC03D'
-      },
-      backgroundImage: {
-        fondoRedes: "url('../image/fondoRedes')"
-      }
-    },
-  },
-      fontFamily: {
-        bebasNeue:["Bebas Neue"]
-      },
-=======
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
         "celeste": "#e7eefb",
-        "violeta": "#432344" 
+        "violeta": "#432344",
+        "violet": '#432344',
+        "red": '#FF2525',
+        "grey": '#E7EEFB',
+        "yellow": '#FFC03D'
       }
     },
   },

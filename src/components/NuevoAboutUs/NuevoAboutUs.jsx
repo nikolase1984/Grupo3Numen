490e8ac40c7eef37b4151576509b2e259718ba29
exequiel <exequielagustindefreitas@gmail.com>
2023-05-02 15:25:31 -0300

    cambios

--- src/components/NuevoAboutUs/NuevoAboutUs.jsx
+++ src/components/NuevoAboutUs/NuevoAboutUs.jsx
@@ -1,12 +1,14 @@
+import * as style from "../NuevoAboutUs/styles"
+
 const NuevoAboutUs = () => {
     return (
-      <div className="py-8 bg-black">
-        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="sm:flex sm:items-center sm:justify-between">
-            <h2 className="text-3xl font-bold text-white sm:mb-4">Acerca de nosotros</h2>
-            <p className="mb-4 text-gray-500 sm:ml-6 sm:mb-0">Somos un comercio local de pastas frescas y deliciosas.</p>
+      <div className={style.divContainer}>
+        <div className={style.div}>
+          <div className={style.titulos}>
+            <h2 className={style.h2}>Acerca de nosotros</h2>
+            <p className={style.p}>Somos un comercio local de pastas frescas y deliciosas.</p>
           </div>
-          <div className="mt-8 text-base text-white sm:text-lg">
+          <div className={divTexto}>
             <p className="mb-4">En nuestra tienda, utilizamos ingredientes frescos y de alta calidad para crear pastas deliciosas y únicas. Nuestro equipo de chefs experimentados trabaja duro para crear sabores que satisfagan a todos los gustos.</p>
             <p className="mb-4">Ofrecemos una amplia variedad de pastas, desde las opciones clásicas hasta las más creativas, como la pasta de remolacha y la pasta de tinta de calamar.</p>
             <p className="mb-4">Además, nuestro servicio de entrega rápida y confiable garantiza que nuestros clientes puedan disfrutar de nuestras pastas frescas en la comodidad de su hogar. Siempre estamos disponibles para responder cualquier pregunta o inquietud que puedan tener nuestros clientes.</p>
---
+++ src/components/NuevoAboutUs/styles.js
@@ -0,0 +1,3 @@
+export const divContainer = "py-8 bg-black"
+export const div ="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8"
+export const titulos ="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8"

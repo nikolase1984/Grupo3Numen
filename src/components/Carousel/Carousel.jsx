82426a5c71cce44a29d8e5f7b90599ccff053a85
exequiel <exequielagustindefreitas@gmail.com>
2023-05-06 19:15:11 -0300

    modificacion carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,32 +4,36 @@ import '../Carousel/Carousel.css'
 export function Carrusel() {
   return (
     <div>
-      <Carousel className='first'>
-        <Carousel.Item>
-          <img 
-            className="d-block" 
-            src="https://hips.hearstapps.com/prima.cdnds.net/assets/15/26/1435331606-different-pasta-shapes.jpg"
-            alt="First slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item >
-          <img 
-            className="d-block"
-            src="https://assets.epicurious.com/photos/606b6eb818ac0b1841b3bbbf/6:4/w_4526,h_3017,c_limit/WackyPastaShapes_HERO_040121_12258.jpg"
-            alt="Second slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img 
-            className="d-block"
-            src="https://www.finedininglovers.it/sites/g/files/xknfdk1106/files/2022-06/tipi%20di%20pasta%20-%20Finedininglovers.jpg"
-            alt="Third slide"
-          />
-
-        </Carousel.Item>
-      </Carousel>
+      <h3 className='pt-5 text-xl font-bold'>Galeria</h3>
+    <div className='flex items-center content-center w-full h-auto'>
+      <div className='py-8 mx-auto w-96 top-4'>
+        <Carousel className='first'>
+          <Carousel.Item className='text-center'>
+            <img 
+              className='mx-auto d-block' 
+              src='https://hips.hearstapps.com/prima.cdnds.net/assets/15/26/1435331606-different-pasta-shapes.jpg'
+              alt='First slide'
+            />
+          </Carousel.Item>
+          <Carousel.Item className='text-center'>
+            <img 
+              className='mx-auto d-block'
+              src='https://assets.epicurious.com/photos/606b6eb818ac0b1841b3bbbf/6:4/w_4526,h_3017,c_limit/WackyPastaShapes_HERO_040121_12258.jpg'
+              alt='Second slide'
+            />
+          </Carousel.Item>
+          <Carousel.Item className='text-center'>
+            <img 
+              className='mx-auto d-block'
+              src='https://www.finedininglovers.it/sites/g/files/xknfdk1106/files/2022-06/tipi%20di%20pasta%20-%20Finedininglovers.jpg'
+              alt='Third slide'
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </div>
-  )
+    </div>
+  );
 }
 
 export default Carrusel;

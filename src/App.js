05236592041f75bd176ddff58aa41581721e72ab
Nico Escobar <nikolase@gmail.com>
2023-05-06 20:41:17 -0300

    modificaciones en componentes para responsive

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import Footer from "./components/Footer.js";
 import Nav from "./components/NuevaNav/Nav.jsx";
 import BuyNow from "./components/BuyNow";
 
-import Imagenconboton from "./components/Imagen-con-boton";
 import Imagenconboton from "./components/ImagenConBoton/Imagen-con-boton";
-import Pastas from "./components/Cards";
-
 import Info from "./components/Info";
 import CardList from "./components/CardList";
 import CallToAction2 from "./components/CallToAction2/CallToAction2";
@@ -20,11 +17,8 @@ function App() {
     <div className="App">
       <Nav />
       <Imagenconboton />
-
       <TarjetaList />
-     
       <CardList />
-
       <NuevoAboutUs />
       <CallToAction2 />
       <BuyNow />
--- src/components/BuyNow.js
+++ src/components/BuyNow.js
@@ -1,19 +1,31 @@
-import React from 'react'
-import buyNow from "../image/buyNow.png"
-import "../stylesheet/BuyNow.css"
+import React from "react";
+import buyNow from "../image/buyNow.png";
 
 const BuyNow = () => {
   return (
-    <div className="buyNow">
-        <div>
-            <img className="rounded-lg" src={buyNow} alt="buyNow" />
-        </div>
-        <div className="action">
-            <p>Ofrecemos una amplia variedad de pastas frescas y deliciosas elaboradas con ingredientes de alta calidad. Desde opciones sin gluten hasta pastas veganas y vegetarianas, nos enorgullece ofrecer pastas que satisfacen cualquier necesidad dietética o preferencia. Además, nos preocupamos por el medio ambiente y utilizamos empaques biodegradables y fuentes de energía renovable. </p>
-            <a className='raunded-md' href="#tienda">Ir a la tienda</a>            
-        </div>
+    <div className="flex flex-col flex-wrap items-center w-screen my-20 md:my-40 bg-grey md:flex-row">
+      <div className="h1/2 md:w-1/2 ">
+        <img
+          className="object-cover w-full opacity-75 md:h-full "
+          src={buyNow}
+          alt="buyNow"
+        />
+      </div>
+      <div className="h-64 mt-2 font-serif italic md:w-1/2 md:h-auto">
+        <p className="px-6 py-2 text-sm text-center text-black md:text-xl md:text-center md:px-10">
+          Ofrecemos una amplia variedad de pastas frescas y deliciosas
+          elaboradas con ingredientes de alta calidad. Desde opciones sin gluten
+          hasta pastas veganas y vegetarianas, nos enorgullece ofrecer pastas
+          que satisfacen cualquier necesidad dietética o preferencia. Además,
+          nos preocupamos por el medio ambiente y utilizamos empaques
+          biodegradables y fuentes de energía renovable.{" "}
+        </p>
+        <button className="p-2 m-3 rounded-md text-zinc-100 bg-violet md:hover:scale-105 hover:ease-linear">
+          <p>Ir a la tienda</p>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BuyNow
+export default BuyNow;

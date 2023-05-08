b8c3487ecf8eea0c22f99124e02c5f88c4738f28
exequiel <exequielagustindefreitas@gmail.com>
2023-05-07 22:00:49 -0300

    carousel intento push

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Footer from "./components/Footer.js";
 import Nav from "./components/NuevaNav/Nav.jsx";
 import BuyNow from "./components/BuyNow";
 
-import Imagenconboton from "./components/Imagen-con-boton";
+
 import Imagenconboton from "./components/ImagenConBoton/Imagen-con-boton";
-import Pastas from "./components/Cards";
+
 
 import Info from "./components/Info";
 import CardList from "./components/CardList";

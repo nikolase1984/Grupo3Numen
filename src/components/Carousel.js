d2851daace2958796cf1ed0935b82712954cf405
tdelgio <tomas.dg@me.com>
2023-05-07 15:48:22 -1000

    redireccionamiento de la navbar

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,36 +1,34 @@
-import Carousel from 'react-bootstrap/Carousel'
-import '../stylesheet/Carousel.css'
+import Carousel from "react-bootstrap/Carousel";
+import "../stylesheet/Carousel.css";
 
 export function Carrusel() {
   return (
-    <div>
+    <div id="galeria">
       <Carousel>
         <Carousel.Item>
-          <img 
+          <img
             className="d-block"
             src="https://hips.hearstapps.com/prima.cdnds.net/assets/15/26/1435331606-different-pasta-shapes.jpg"
             alt="First slide"
           />
         </Carousel.Item>
         <Carousel.Item>
-          <img 
+          <img
             className="d-block"
             src="https://assets.epicurious.com/photos/606b6eb818ac0b1841b3bbbf/6:4/w_4526,h_3017,c_limit/WackyPastaShapes_HERO_040121_12258.jpg"
             alt="Second slide"
           />
-
         </Carousel.Item>
         <Carousel.Item>
-          <img 
+          <img
             className="d-block"
             src="https://www.finedininglovers.it/sites/g/files/xknfdk1106/files/2022-06/tipi%20di%20pasta%20-%20Finedininglovers.jpg"
             alt="Third slide"
           />
-
         </Carousel.Item>
       </Carousel>
     </div>
-  )
+  );
 }
 
 export default Carrusel;
--- src/components/NuevaNav/Nav.jsx
+++ src/components/NuevaNav/Nav.jsx
@@ -2,35 +2,31 @@ import { useState } from "react";
 import Button from "../NuevaNav/Button";
 import logo from "../../image/Logo blanco.png";
 import Icono from "../../image/barra-de-menus.png";
-import * as style from "../NuevaNav/styles"
+import * as style from "../NuevaNav/styles";
 
 const Navbar = () => {
   let Links = [
     { name: "HOME", link: "/" },
     { name: "PRODUCTOS", link: "/" },
-    { name: "ABOUT US", link: "/" },
-    { name: "GALERIA", link: "/" },
+    { name: "ABOUT US", link: "#about" },
+    { name: "GALERIA", link: "#galeria" },
     { name: "CONTACTOS", link: "/" },
   ];
   let [open, setOpen] = useState(false);
   return (
-    <div className= { style.nav }>
-      <div className={ style.div2 }>
-        <div
-          className={ style.div3 }
-        >
-          <span className={ style.span }>
+    <div className="fixed top-0 z-50">
+      <div className={style.div2}>
+        <div className={style.div3}>
+          <span className={style.span}>
             <img src={logo} alt="logo" className="w-8 h-auto" />
           </span>
         </div>
 
-        <div
-          onClick={() => setOpen(!open)}
-          className={ style.divOnClick }
-        >
-          <button className="w-10" name={open ? "close" : "menu"}><img  src={ Icono } alt="icono" /></button>
+        <div onClick={() => setOpen(!open)} className={style.divOnClick}>
+          <button className="w-10" name={open ? "close" : "menu"}>
+            <img src={Icono} alt="icono" />
+          </button>
         </div>
-        
 
         <ul
           className={`md:flex bg-violet md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${

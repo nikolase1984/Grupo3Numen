f37bd8039bf820a0715c33a69219c56ff44d8238
bzamparo80 <bzamparo@gmail.com>
2023-05-13 21:46:34 -0300

    Arreglo carrito para eliminar por unidad

--- src/components/Tarjetas/Carrito.jsx
+++ src/components/Tarjetas/Carrito.jsx
@@ -17,9 +17,21 @@ export const useCarrito = () => {
   };
 
   const eliminarDelCarrito = (productoAEliminar) => {
-    setCarrito(
-      carrito.filter((producto) => producto.id !== productoAEliminar.id)
-    );
+    const index = carrito.findIndex((producto) => producto.id === productoAEliminar.id);
+    if (index !== -1) {
+      // Encontrar el producto y disminuir su cantidad en 1
+      const nuevoCarrito = [...carrito];
+      nuevoCarrito[index].cantidad -= 1;
+  
+      // Si la cantidad resultante es mayor a cero, actualizar el carrito
+      if (nuevoCarrito[index].cantidad > 0) {
+        setCarrito(nuevoCarrito);
+      } else {
+        // De lo contrario, eliminar el producto del carrito por completo
+        nuevoCarrito.splice(index, 1);
+        setCarrito(nuevoCarrito);
+      }
+    }
   };
 
   // Obtener el contador total del carrito
